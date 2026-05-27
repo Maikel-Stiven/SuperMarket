31e70ea13e43b14c8ec73987003aefce50f709ee
Maikel-St <maikystiven.777@gmail.com>
2026-05-27 11:15:44 -0500

    feat. se implemento interfaz y lógica de negocio para empleados

---
+++ src/main/java/com/example/SuperMarket/modules/personal/services/EmployeeService.java
@@ -0,0 +1,16 @@
+package com.example.SuperMarket.modules.personal.services;
+
+import com.example.SuperMarket.modules.personal.dtos.EmployeeRequestDTO;
+import com.example.SuperMarket.modules.personal.dtos.EmployeeResponseDTO;
+
+import java.util.List;
+import java.time.LocalDate;
+
+public interface EmployeeService {
+    
+    EmployeeResponseDTO createEmployee (EmployeeRequestDTO request);
+    EmployeeResponseDTO getEmployeeById (String id);
+    List<EmployeeResponseDTO> getAllEmployees();
+    List<EmployeeResponseDTO> getEmployeesByRole (String role);
+    List<EmployeeResponseDTO> getEmployeeByHiredDateRange (LocalDate startDate, LocalDate endDate);
+}
